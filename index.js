3b8295f1e9a443f5e36c3e3b56069bfd666185b0
nishadahmed5 <nishatahmed650@gmail.com>
2022-10-26 16:27:12 +0600

    category wise data load

--- index.js
+++ index.js
@@ -5,6 +5,23 @@ const cors = require('cors')
 app.use(cors())
 const courseCategory =  require('./data/course-category.json')
 
+const courses = require('./data/courses.json')
+
+app.get('/courses', (req, res)=>{
+    res.send(courses)
+})
+
+app.get('/category/:id', (req, res)=>{
+    const id = req.params.id;
+    const course_category = courses.filter(cat=> cat.category_id === id)
+    res.send(course_category)
+})
+app.get('/course-details/:id', (req, res)=>{
+    const id = req.params.id;
+    const details = courses.find( d=> d._id === id)
+    res.send(details)
+})
+
 app.get('/', (req, res)=>{
     res.send('APi is Running')
 })
